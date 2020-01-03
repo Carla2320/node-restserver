7cd9eb2d98c0ca44ca52abc7da4a49ecd2bdd2d8
Javier Fernandez <cguamanzara@est.ups.edu.ec>
2020-01-03 12:37:50 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+require('./config/config')
+    //capa media donde transforma en json
+
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', function(req, res) {
+    res.json("get usuario");
+
+});
+
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+                ok: false,
+                mensaje: "El nombre es necesario"
+            }
+
+        );
+    } else {
+        res.json({
+            usuario: body
+        })
+
+    }
+
+});
+//RECIBES UN PARAMETRO
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id
+    res.json({
+        cedula: id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json("delete usuario");
+
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("escuchando en el puerto ", process.env.PORT);
+})
